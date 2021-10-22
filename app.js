29c6995378b3b33f925280e51365c0b16cd6a00f
abhishekbisht10 <abisht2001@gmail.com>
2021-10-22 14:50:31 +0530

    add email validation

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {check, validationResult} = require('express-validator');
 
-const app = express()
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req, res) { 
-    res.render("home")
-})
+    res.render("home");
+});
 
 app.get("/shows/:id", function(req, res) {
-    let show = req.params.id
-    res.render("shows", {showId: show})
-})
+    let show = req.params.id;
+    res.render("shows", {showId: show});
+});
 
 app.get("/podcast", function(req, res) {
-    res.render("podcast")
-})
+    res.render("podcast");
+});
 
 app.get("/about", function(req, res) {
-    res.render("about")
-})
+    res.render("about");
+});
 
-app.post("/contact-form", function(req, res) {
+app.post("/about", [
+    check("name", "Name must be 4+ characters long").exists().isLength({min: 4}),
+    check("email", "Enter a valid e-mail address").isEmail().normalizeEmail()
+], function(req, res) {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const alert = errors.array();
+        res.render("about", {alert: alert});
+    }
+    
+    else {
     let name = req.body.name;
-    res.redirect("/about")
-})
+    let email = req.body.email;
+    let message = req.body.message;
+    res.redirect("/about");
+    }
+});
 
 app.listen(3000, function() {
-    console.log("Server started at port 3000")
-})
+    console.log("Server started at port 3000");
+});
